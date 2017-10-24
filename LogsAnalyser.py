6bc04189efd8010a0edc9e211dc0c9b3ae2106ee
Juan De la Rosa <j.delarosa48@gmail.com>
2017-10-24 07:52:44 -0400

    change some functions behaviors to work with the new queries results

--- LogsAnalyser.py
+++ LogsAnalyser.py
@@ -1,26 +1,19 @@
+#!/usr/bin/env python3
 import dbModules
 import datetime
-
-
-db_conn_string = "dbname=news"
 
 
 def error_day_percentage(data):
     """ Print all the days that have more than 1% of fail
     requests """
     for row in data:
-        if float(row[1]) > 1.0:
-            print("*", datetime.datetime.strptime(
-                row[0], '%Y-%m-%d').strftime('%B %d, %Y'),
-                "- {:.2f}% errors".format(float(row[1])))
+        print('* {0:%B %d, %Y} - {1:.2f}% error'.format(row[0], row[1]))
 
 
 def popular_three_articles(data):
     """ Print the three articles with more views """
-    for row in range(len(data)):
-        if row == 3:
-            return
-        print("*", data[row][0], "-", data[row][1], "views")
+    for row in data:
+        print("*", row[0], "-", row[1], "views")
 
 
 def popular_authors(data):
@@ -28,18 +21,18 @@ def popular_authors(data):
     for row in (data):
         print("*", row[0], "-", row[1], "views")
 
-
-print("What are the most popular three articles of all time?")
-print()
-data = dbModules.get_articles_views(db_conn_string)
-popular_three_articles(data)
-print()
-print("Who are the most popular article authors of all time?")
-print()
-data = dbModules.get_authors_views(db_conn_string)
-popular_authors(data)
-print()
-print("On which days did more than 1% of requests lead to errors?")
-print()
-data = dbModules.get_error_count(db_conn_string)
-error_day_percentage(data)
+if __name__ == '__main__':
+    print("What are the most popular three articles of all time?")
+    print()
+    data = dbModules.get_articles_views()
+    popular_three_articles(data)
+    print()
+    print("Who are the most popular article authors of all time?")
+    print()
+    data = dbModules.get_authors_views()
+    popular_authors(data)
+    print()
+    print("On which days did more than 1% of requests lead to errors?")
+    print()
+    data = dbModules.get_error_count()
+    error_day_percentage(data)
